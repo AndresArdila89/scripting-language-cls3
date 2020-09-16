e5473f8ba589f43a064f4888ec1bbda626098071
AndresArdila89 <andresardila@me.com>
2020-09-16 09:49:09 -0400

    first commit

---
+++ strings.py
@@ -0,0 +1,20 @@
+s1 = 'Introduction to Python'
+print(s1)
+print(s1[0])
+
+print(s1[5])
+print(s1[-1])
+print(s1[-2])
+print(s1[-7])
+
+print(s1[0:5])
+print(s1[:13])
+print(s1[16:-1])
+print(s1[-2:-5])###retun and empty char
+
+s1 = s1 + " for students"
+print(s1)
+##print(s1 + 2)  error
+##print(s1 * 2)
+
+print(s1[0:5] + 'D' + [7:20])
