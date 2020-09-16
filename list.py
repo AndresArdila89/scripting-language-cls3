78c151c0defd31ebd6ab34e50139a42726853519
AndresArdila89 <andresardila@me.com>
2020-09-16 11:15:22 -0400

    sort method

--- list.py
+++ list.py
@@ -24,3 +24,21 @@ del[L3] ##delete the list
 L4 = ['Math', 'Algebra', 'Art']
 L4.append('Comp Sci')
 print(L4)
+L4.append('Comp Sci')
+L4.append('Math')
+L4.append('Algebra')
+print(L4)
+print(L4.index('Math'))
+print(L4.count('Math'))
+L5 = ['English', 'French']
+L4.extend(L5)
+print(L4)
+L4.removeL('Math')
+print(L4)
+L4.insert(3,'Design')
+print(L4)
+
+###L4.sort() this method affects the object
+###print(L4)
+
+print(sort(L4)) ##3this way the object does not get affected by the method sort()
